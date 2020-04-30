16840c8b7520dcfc4506a78040b89110c8ca8100
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-30 02:15:07 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+   def initialize(name,breed)
+    @name = name
+  end
+ 
+  def dog(name)
+    @name = name
+  end
+  def dog
+    @name
+  end
+end
+ def dog(breed)
+    @breed= breed
+  end
+  def dog
+    @breed
+  end
+ fido = Dog.new("Fido", "Pug")
+
+ name.breed
+ 
+  
+  
